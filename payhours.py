6ef156c59d1277d031a0821e2f1e0a5ab44c9a3f
Tal Livny <livnytal.dev@gmail.com>
2017-11-26 14:32:46 +0200

    Windows exe, removed colorama

--- payhours.py
+++ payhours.py
@@ -1,12 +1,15 @@
 import datetime,colorama
+
+REQUIRED_WORKING_HOURS_EACH_DAY = 9
+
 now = datetime.datetime.now()
 today = now.today().day
-
-holidays = [] #[datetime.date(2013, 8, 14)] -add holidays here
+alreadyDone = int(raw_input("Enter number of hours already done: \n"))
+holidays = [datetime.date(2017, 11, 22)] #[] -add holidays here
 restDays = [4,5]
 businessdays = 0
-REQUIRED_WORKING_HOURS_EACH_DAY = 9
 lastday = 0
+
 for i in range(1, 32):
     try:
         thisdate = datetime.date(now.year, now.month, i)
@@ -17,12 +20,14 @@ for i in range(1, 32):
         businessdays += 1
 total = businessdays * REQUIRED_WORKING_HOURS_EACH_DAY
 
-alreadyDone = 126.35 # Change this every day
-dayleft = lastday - today
+dayleft = lastday - today + 1
+
+
 print "---------------------------"
-print colorama.Fore.YELLOW + "Total number of hours to work this month: {}".format(total)
-print colorama.Fore.GREEN + "Total number of hours already done this month: {}".format(alreadyDone)
-print colorama.Fore.RED + "Number of hours left to work this month: {}".format(total-alreadyDone)
-print colorama.Fore.LIGHTRED_EX + "Number of days left to work this month: {}".format(dayleft)
-print colorama.Fore.MAGENTA + "Average of hours left to work each day this month: {}".format((total-alreadyDone)/float(dayleft))
-print colorama.Fore.RESET + "---------------------------"
+print "Total number of hours to work this month: {}".format(total)
+print "Total number of hours already done this month: {}".format(alreadyDone)
+print "Number of hours left to work this month: {}".format(total-alreadyDone)
+print "Number of days left to work this month: {}".format(dayleft)
+print "Average of hours left to work each day this month: {}".format((total-alreadyDone)/float(dayleft))
+print "---------------------------"
+raw_input("Press any key to exit...")
